981f57dcd33b59692bfcb502a6a43c5880b4b637
danieljennings817-git <danieljennings817@gmail.com>
2025-10-16 16:26:30 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+// next.config.js
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 /** @type {import('next').NextConfig} */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  turbopack: { root: __dirname },
+  turbopack: { root: __dirname }, // works in ESM now
 };
+
 export default nextConfig;
+
